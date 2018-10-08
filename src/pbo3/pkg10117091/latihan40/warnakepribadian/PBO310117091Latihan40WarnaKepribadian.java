7f060ec9afa8ede19741fdebde893c760511a8a4
Dida Handian <handiandida@gmail.com>
2018-10-08 22:40:48 +0700

    WarnaKepribadian

---
+++ src/pbo3/pkg10117091/latihan40/warnakepribadian/PBO310117091Latihan40WarnaKepribadian.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan40.warnakepribadian;
+
+import java.util.Scanner;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : 
+*/
+public class PBO310117091Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Warna wrn = new Warna();
+        User usr = new User();
+        Scanner scn = new Scanner(System.in);
+
+        wrn.menampilkanTemplate();
+        wrn.pilihWarnaFavorit();
+        usr.memasukkanNama();
+
+        System.out.println("\n====HASIL TEST KEPRIBADIAN "+ usr.namaUser.toUpperCase()+"====");
+        wrn.tesKepribadian(wrn.namaWarna,usr.namaUser);
+
+    }
+    
+}
